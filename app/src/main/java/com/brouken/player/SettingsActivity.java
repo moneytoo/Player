7664143db2738a138dbaa3cb6ef50d4b42a5a4b3
Marcel Dopita <m@rcel.cz>
2021-09-18 10:19:43 +0200

    Transparent navigation bar in settings

--- app/src/main/java/com/brouken/player/SettingsActivity.java
+++ app/src/main/java/com/brouken/player/SettingsActivity.java
@@ -1,17 +1,33 @@
 package com.brouken.player;
 
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SettingsActivity extends AppCompatActivity {
 
+    static RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        if (Build.VERSION.SDK_INT >= 29) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            );
+            getWindow().setNavigationBarColor(Color.TRANSPARENT);
+        }
+
         super.onCreate(savedInstanceState);
 
         Prefs.initDefaults(this);
@@ -27,6 +43,21 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
+        if (Build.VERSION.SDK_INT >= 29) {
+            LinearLayout layout = findViewById(R.id.settings_layout);
+            layout.setOnApplyWindowInsetsListener((view, windowInsets) -> {
+                view.setPadding(windowInsets.getSystemWindowInsetLeft(),
+                        windowInsets.getSystemWindowInsetTop(),
+                        windowInsets.getSystemWindowInsetRight(),
+                        0);
+                if (recyclerView != null) {
+                    recyclerView.setPadding(0,0,0, windowInsets.getSystemWindowInsetBottom());
+                }
+                windowInsets.consumeSystemWindowInsets();
+                return windowInsets;
+            });
+        }
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
@@ -41,6 +72,14 @@ public class SettingsActivity extends AppCompatActivity {
             Preference preferenceFrameRateMatching = findPreference("frameRateMatching");
             if (preferenceFrameRateMatching != null) {
                 preferenceFrameRateMatching.setEnabled(Build.VERSION.SDK_INT >= 23);
+            }
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+            if (Build.VERSION.SDK_INT >= 29) {
+                recyclerView = getListView();
             }
         }
     }
